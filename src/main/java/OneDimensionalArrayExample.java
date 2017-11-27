784aa1f5d9b21c56ac5a559e481375e398be16ca
Kyli McKay-Bishop <kmckay-bishop@commandalkon.com>
2017-11-27 16:30:25 -0700

    Chapter 5 initial examples

---
+++ src/main/java/OneDimensionalArrayExample.java
@@ -0,0 +1,19 @@
+public class OneDimensionalArrayExample {
+
+	public static void main(String[] args) {
+		int[] numbers = new int[10]; // Declare new array using single statement
+
+		int[] numbers2;
+		numbers2 = new int[10]; // Declare new array using two statements
+
+		for(int i = 0; i < 10; i++) { // Initialize using for loop
+			numbers[i] = i;
+			numbers2[i] = i;
+		}
+
+		// Declare and initialize in single line
+		int[] numbers3 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+	}
+
+}
